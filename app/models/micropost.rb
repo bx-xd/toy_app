936ed192774994f1d94a845156ea23bed9026d96
bX_Xd <a.brique@protonmail.com>
2021-08-26 11:49:43 +0200

    fixing presence micropost's model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,4 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { minimum: 8, maximum: 140 }, presence :true
+  validates :content, length: { minimum: 8, maximum: 140 }, presence: true
 end
